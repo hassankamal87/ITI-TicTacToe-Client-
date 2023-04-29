7327a0ca576e3b699eeb38416dbfe03ac4b31233
mohamedadel2323 <mohamedadel2323m@gmail.com>
2023-04-29 20:55:37 +0200

    Created controller for pick your side screen and handled buttons navigation to it from main screen and back to screen

--- src/tictactoe/XML/MainScreenUiController.java
+++ src/tictactoe/XML/MainScreenUiController.java
@@ -5,14 +5,19 @@
  */
 package tictactoe.XML;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -44,7 +49,7 @@ public class MainScreenUiController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
+
 //        computerBtn.addEventHandler(ActionEvent.ACTION,
 //                new EventHandler<ActionEvent>() {
 //            @Override
@@ -54,13 +59,27 @@ public class MainScreenUiController implements Initializable {
 //            }
 //        }
 //        );
-        
-        
-    }    
+    }
 
     @FXML
-    private void playWithComputer(ActionEvent event) {
-      
+    private void playWithComputer(ActionEvent event) throws IOException {
+        PickYourSideScreenController.setMode(PickYourSideScreenController.COMPUTER_MODE);
+        gotoPickSide();
     }
-    
+
+    @FXML
+    private void playlocal(ActionEvent event) throws IOException {
+        PickYourSideScreenController.setMode(PickYourSideScreenController.LOCAL_MODE);
+        gotoPickSide();
+    }
+
+    private void gotoPickSide() throws IOException {
+        System.out.println(PickYourSideScreenController.getMode()+"");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PickYourSideScreen.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) computerBtn.getScene().getWindow();
+        stage.setScene(scene);
+    }
+
 }
---
+++ src/tictactoe/XML/PickYourSideScreenController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.XML;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Mohamed Adel
+ */
+public class PickYourSideScreenController implements Initializable {
+    public static int COMPUTER_MODE = 0;
+    public static int LOCAL_MODE = 1;
+    private static int gameMode;
+    @FXML
+    private ImageView xImg;
+    @FXML
+    private ImageView oImg;
+    @FXML
+    private ImageView backImg;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backToMain(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScreenUi.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) backImg.getScene().getWindow();
+        stage.setScene(scene);
+    }
+    
+    public static void setMode(int mode){
+        gameMode = mode;
+    }
+    public static int getMode(){
+        return gameMode;
+    }
+    
+}
