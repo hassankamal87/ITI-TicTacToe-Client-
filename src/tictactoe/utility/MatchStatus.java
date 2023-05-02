9210cd724ab7fcc208e5f5d6e5744aa8d41bf645
mdanash97 <132082850+mdanash97@users.noreply.github.com>
2023-05-02 13:03:28 +0200

    Result screen modify v4.31

---
+++ src/tictactoe/utility/MatchStatus.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.utility;
+
+/**
+ *
+ * @author WIN 10
+ */
+public enum MatchStatus {
+    WIN,
+    LOSE,
+    DRAW
+}
