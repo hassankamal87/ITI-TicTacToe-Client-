170b03dfed6fbc882785ec2f24bfd6d0ecfb37b2
mdanash97 <132082850+mdanash97@users.noreply.github.com>
2023-05-08 17:32:31 +0200

    Client SignIn v2

--- src/tictactoe/controller/SignInScreenController.java
+++ src/tictactoe/controller/SignInScreenController.java
@@ -6,20 +6,28 @@
 package tictactoe.controller;
 
 import java.io.IOException;
+import java.net.SocketException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import org.json.simple.JSONObject;
+import tictactoe.Connection;
+import tictactoe.utility.JsonObjectHelper;
 
 /**
  * FXML Controller class
@@ -37,35 +45,93 @@ public class SignInScreenController implements Initializable {
     @FXML
     private PasswordField passwordTextField;
 
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    String email;
+    String password;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void signInHandler(ActionEvent event) {
+        Connection connection = Connection.getInstance();
+        String resultString = validation();
+        if (resultString.equals("")) {
+            JSONObject signinObject = new JSONObject();
+            signinObject.put(JsonObjectHelper.HEADER, JsonObjectHelper.LOGIN);
+            signinObject.put(JsonObjectHelper.EMAIL, email);
+            signinObject.put(JsonObjectHelper.PASSWORD, password);
+            new Thread() {
+                @Override
+                public void run() {
+                    if (!connection.isConnected()) {
+                        try {
+                            connection.startConnection();
+                        } catch (SocketException ex) {
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    alert.setContentText("Sorry, Server Is Closed Right Now");
+                                    alert.showAndWait();
+                                }
+                            });
+
+                        }
+                    }
+                    //to avoid exception 
+                    if (connection.isConnected()) {
+                        connection.getPrintStream().println(signinObject);
+                    }
+                }
+            }.start();
+        } else {
+            alert.setContentText(resultString);
+            alert.show();
+        }
+
     }
 
     @FXML
     private void backHandler(ActionEvent event) throws IOException {
-        
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/tictactoe/XML/MainScreenUi.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root,610,410);
-            Stage stage = (Stage) backBtn.getScene().getWindow();
-            stage.setScene(scene);
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 610, 410);
+        Stage stage = (Stage) backBtn.getScene().getWindow();
+        stage.setScene(scene);
     }
 
     @FXML
     private void goToSignup(MouseEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/tictactoe/XML/SignUPScreen.fxml"));
-            Parent root = loader.load();
-            Scene scene = new Scene(root,610,410);
-            Stage stage = (Stage) backBtn.getScene().getWindow();
-            stage.setScene(scene);
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 610, 410);
+        Stage stage = (Stage) backBtn.getScene().getWindow();
+        stage.setScene(scene);
     }
-    
+
+    private String validation() {
+        String errorMessage = "";
+        email = userNameTextField.getText().trim();
+        password = passwordTextField.getText().trim();
+
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (email.isEmpty() || password.isEmpty()) {
+            errorMessage = "All fields are required";
+        } else if (!matcher.matches()) {
+            errorMessage = "Enter Valid Email Please";
+        } else if (password.length() < 8) {
+            errorMessage = "Password can't be less than 8 symbols";
+        }
+        return errorMessage;
+    }
+
 }
